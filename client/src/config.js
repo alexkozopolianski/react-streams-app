af9e477e1db3ec241b9d4c76865d4baa5c94d42e
Alex Kozopolianski <42234681+alexkozopolianski@users.noreply.github.com>
2021-03-15 14:08:29 +0300

    Update config.js

--- client/src/config.js
+++ client/src/config.js
@@ -4,14 +4,7 @@ import 'firebase/database';
 import 'firebase/storage';
 
 const config = {
-    apiKey: "AIzaSyCaimatc4sFXQGU4cchzX7T43Q0SqpRMB8",
-    authDomain: "exampleeee-5f208.firebaseapp.com",
-    databaseURL: "https://exampleeee-5f208.firebaseio.com",
-    projectId: "exampleeee-5f208",
-    storageBucket: "exampleeee-5f208.appspot.com",
-    messagingSenderId: "323335924575",
-    appId: "1:323335924575:web:7cbe29dc137ca59c4a64ea",
-    measurementId: "G-R3M59W1EJ7"
+   you firebase config
   };
 
   firebase.initializeApp(config);
